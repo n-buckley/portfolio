e37b6b739d7dcbbb2fc94d5290a0c20c3f35a43c
Nicole Buckley <43097855+n-buckley@users.noreply.github.com>
2022-01-28 18:24:06 -0500

    Basic landing page, name centered

---
+++ src/components/pages/Landing/LandingPage.jsx
@@ -0,0 +1,13 @@
+import './landing.css';
+
+function LandingPage() { 
+    return(
+        <div className = "page"> 
+            <div className="t-wrapper">
+                <h1 className="title">Nicole Buckley</h1>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
