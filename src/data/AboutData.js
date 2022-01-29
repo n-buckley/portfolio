aed0007e64d7dcf321b677c16ff28c46390b419d
Nicole Buckley <43097855+n-buckley@users.noreply.github.com>
2022-01-29 01:22:51 -0500

    Data files for about and proects

---
+++ src/data/AboutData.js
@@ -0,0 +1,11 @@
+
+export const About = {
+    id: 1,
+    about: [
+        "Hello! My name is Nicole Buckley", 
+        "I blurb sentence here and her is where a blub about me would go about what I professionaly enjoy doing! and other things that I like doing and intersection of this and that and i am so cool", 
+        "I also like ! all of these things like lattes and hiking and other exciting nature thingies and wow im so quircky and different",
+        "These are some technologies I’ve recently worked with:"
+    ], 
+    skills: ["React", "Java", "Python", "CSS"]
+}
---
+++ src/data/ProjectData.js
@@ -0,0 +1,24 @@
+//TODO: makybe suppor an "internal"link e.g. you make a blog post that you host on your site 
+//explaing the process of the project or whaterver idk.
+// Wouldnt be inlink because if you click anywhere on the card it shuod go
+
+export const Projects = [
+    {
+        id: 1,
+        title: "Project Title 1",
+        desc: "I blurb sentence here and her is where a blub about me would go about what I professionaly enjoy doing! and other things that I like doing and intersection of this ",
+        tech: ["React", "HTML", "CSS"],
+        linksKey: ['github', 'external'],
+        links: ['github.com', 'google.com']
+    },
+
+    {
+        id: 2,
+        title: "Project Title 2",
+        desc: "I blurb sentence here and her is where a blub about me would go about what I professionaly enjoy doing! and other things that I like doing and intersection of this ",
+        tech: ["React", "CSS"],
+        linksKey: ['github', 'devpost', 'external'],
+        links: ['github.com', 'devpost.com', 'google.com']
+    },
+
+]
